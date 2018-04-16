e84c4a65296fe4c77c4569ec784da05fb21de82e
simxn <171wita27@o365.htl-leoben.at>
2018-04-16 16:31:48 +0200

    added age feature

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,7 @@
 # Hello World mit Python
 name = input("What's your name?")
 print("Hello World!", name)
+
+age = input("How old are you?")
+print("You are ", age, " years old this year")
+print("Next year you will be", int(age) +1)
